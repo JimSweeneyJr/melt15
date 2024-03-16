828fe6034ae1913028b2a8145d3d61387372bf47
Jim Sweeney <131217444+JimSweeneyJr@users.noreply.github.com>
2024-03-15 22:50:43 -0400

    initial commit

---
+++ melt15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  melt15
+//
+//  Created by James Sweeney on 3/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("initial commit")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ melt15/melt15App.swift
@@ -0,0 +1,17 @@
+//
+//  melt15App.swift
+//  melt15
+//
+//  Created by James Sweeney on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct melt15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
